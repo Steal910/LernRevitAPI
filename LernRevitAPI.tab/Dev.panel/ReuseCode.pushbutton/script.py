bb2a7f53cc3095d4de96d94d679aa6864381319c
Steal910 <mateusz.dworzycki@o2.pl>
2024-10-24 17:05:58 +0200

    ⭐ Published LernRevitAPI Extension

---
+++ LernRevitAPI.tab/Dev.panel/ReuseCode.pushbutton/script.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+__title__   = "ReuseCode in pyRevit"
+__doc__     = """Version = 1.0
+Date    = 23.10.2024
+________________________________________________________________
+Last Updates:
+- [23.10.2024] v1.0 Create
+________________________________________________________________
+Author: Mateusz Dworzycki"""
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝
+#==================================================
+from Autodesk.Revit.DB import *
+
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝
+#==================================================
+app    = __revit__.Application
+uidoc  = __revit__.ActiveUIDocument
+doc    = __revit__.ActiveUIDocument.Document #type:Document
+
+
+# ╔╦╗╔═╗╦╔╗╔
+# ║║║╠═╣║║║║
+# ╩ ╩╩ ╩╩╝╚╝
+#==================================================
+
+from Snippets._selection import get_selected_elements
+
+sel_el = get_selected_elements()
+
+sel_walls = get_selected_elements([Wall, Floor])
+
+print(sel_el)
+print(sel_walls)
